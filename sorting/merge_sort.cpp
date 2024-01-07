0fa617facebb2c3f3398e4b8c98aadc784620c7f
badal <badal@badals-MacBook-Air.local>
2024-01-07 20:48:58 +0530

    new

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -1,69 +1,66 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void merge(int *arr,int s,int e)
+void merge(int *arr, int s, int e)
 {
-  int mid=(s+e)/2;
-  int len1=mid-s+1;
-  int len2=e-mid;
+  int mid = (s + e) / 2;
+  int len1 = mid - s + 1;
+  int len2 = e - mid;
 
-  int *first= new int[len1];
-  int *second= new int[len2];
-  int k=s;
-  for(int i=0;i<len1;i++)
+  int *first = new int[len1];
+  int *second = new int[len2];
+  int k = s;
+  for (int i = 0; i < len1; i++)
   {
-    first[i]=arr[k++];
+    first[i] = arr[k++];
   }
-  for(int i=0;i<len2;i++)
+  for (int i = 0; i < len2; i++)
   {
-    second[i]=arr[k++];
+    second[i] = arr[k++];
   }
 
-  int index1=0;
-  int index2=0;
-  k=s;
-  while(index1<len1&&index2<len2)
+  int index1 = 0;
+  int index2 = 0;
+  k = s;
+  while (index1 < len1 && index2 < len2)
   {
-    if(first[index1]<second[index2])
+    if (first[index1] < second[index2])
     {
-      arr[k++]=first[index1++];
+      arr[k++] = first[index1++];
     }
-    else{
-      arr[k++]=second[index2++];
+    else
+    {
+      arr[k++] = second[index2++];
     }
   }
-  while(index1<len1)
+  while (index1 < len1)
   {
-    arr[k++]=first[index1++];
+    arr[k++] = first[index1++];
   }
-  while(index2<len2)
+  while (index2 < len2)
   {
-    arr[k++]=second[index2++];
+    arr[k++] = second[index2++];
   }
-
-
-
-
 }
-void mergesort(int *arr,int s,int e)
+void mergesort(int *arr, int s, int e)
 {
-  if(s>=e)
+  if (s >= e)
   {
     return;
   }
-  int mid=(s+e)/2;
-  mergesort(arr,s,mid);
-  mergesort(arr,mid+1,e);
-  merge(arr,s,e);
+  int mid = (s + e) / 2;
+  mergesort(arr, s, mid);
+  mergesort(arr, mid + 1, e);
+  merge(arr, s, e);
 }
 int main()
 {
-    int arr[5]={2,1,8,4,19};
-    int n=5;
-    mergesort(arr,0,n-1);
-    for(int i=0;i<n;i++)
-    {
-      cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-    return 0;
+  int arr[5] = {2, 1, 8, 4, 19};
+  int n = 5;
+  mergesort(arr, 0, n - 1);
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+  return 0;
 }
